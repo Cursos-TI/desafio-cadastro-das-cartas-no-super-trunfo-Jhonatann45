53de29098925013f94a47fceedd96267347e5883
Jhonatann45 <iurytakeshi@gmail.com>
2025-01-22 05:51:33 +0000

    comit novo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,38 @@
 int main(){
 
 printf("Desafio Xadrez!\n");
-printf("Novo Comit\n");
+
+ char nome[100];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+
+
+
+
+
+printf("Digite O Nome Da Cidade: \n");
+scanf("%s" , nome);
+
+printf("Digite a População Da Cidade: \n ");
+scanf("%d" , &populacao);
+
+printf("Digite A Área Da Cidade(em km²): \n ");
+scanf("%f", &area);
+
+printf("Digite O Pib Da Cidade: \n ");
+scanf("%f" ,  &pib);
+
+printf("Números De Pontos Turísticos: \n ");
+scanf("%d" , &pontos_turisticos);
+
+
+
+
+
+
+
 
 }
 
