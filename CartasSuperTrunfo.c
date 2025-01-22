c7d59af62cea1f4b7a34c1367bd849479979634d
Jhonatann45 <iurytakeshi@gmail.com>
2025-01-22 06:21:24 +0000

    comit final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
 
+
+
+
 int main(){
+
 
 printf("Desafio Xadrez!\n");
 
- char nome[100];
-    int populacao;
-    float area;
-    float pib;
-    int pontos_turisticos;
+char nome[100];
+int populacao;
+float area;
+float pib;
+int pontos_turisticos;
 
+ 
 
+printf("Digite Sua Cidade: \n");
+scanf("%s", &nome);
 
+printf("Digite A População Da Sua Cidade: \n");
+scanf("%d", &populacao);
 
+printf("Digite A Área da sua cidade: \n");
+scanf("%f" , &area);
 
-printf("Digite O Nome Da Cidade: \n");
-scanf("%s" , nome);
+printf("Digite O Pib Da Sua Cidade: \n");
+scanf("%f", &pib);
 
-printf("Digite a População Da Cidade: \n ");
-scanf("%d" , &populacao);
-
-printf("Digite A Área Da Cidade(em km²): \n ");
-scanf("%f", &area);
-
-printf("Digite O Pib Da Cidade: \n ");
-scanf("%f" ,  &pib);
-
-printf("Números De Pontos Turísticos: \n ");
+printf("Numeros De Pontos Turísticos: \n");
 scanf("%d" , &pontos_turisticos);
 
 
 
 
+printf("Nome da Cidade: %s \n " , nome );
+printf("População: %d \n" , populacao);
+printf("Área: %f \n" , area);
+printf("Pib: %f \n" , pib);
+printf("Pontos Turisticos: %d \n" , pontos_turisticos);
 
 
 
+return 0;
 
 }
-
