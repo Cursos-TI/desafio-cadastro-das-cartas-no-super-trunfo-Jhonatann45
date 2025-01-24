d2fc0e172c51d54946dbae882395c2cc44e87fd9
Jhonatann45 <iurytakeshi@gmail.com>
2025-01-24 03:00:15 +0000

    novato finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,7 +6,7 @@
 int main(){
 
 
-printf("Desafio Xadrez!\n");
+printf("\n Cidade 1\n");
 
 char nome[100];
 float populacao;
@@ -19,29 +19,123 @@ int pontos_turisticos;
 printf("Digite Sua Cidade: \n");
 scanf("%s", nome);
 
-printf("Digite A População Da Sua Cidade: \n");
+printf("Digite A População Da Sua Cidade:\n");
 scanf("%f", &populacao);
 
-printf("Digite A Área da sua cidade: \n");
+printf("Digite A Área da sua cidade:\n");
 scanf("%f" , &area);
 
-printf("Digite O Pib Da Sua Cidade: \n");
+printf("Digite O Pib Da Sua Cidade:\n");
 scanf("%f", &pib);
 
-printf("Numeros De Pontos Turísticos: \n");
+printf("Numeros De Pontos Turísticos:\n");
 scanf("%d" , &pontos_turisticos);
 
 
+// Dados da Cidade //
 
-printf("\n Informações Da Cidade: \n");
-printf("Nome da Cidade: %s\n " , nome );
+printf("Nome da Cidade: %s\n "  , nome );
 printf("População: %f\n" , populacao);
 printf("Área: %f \n" , area);
 printf("Pib: %f \n" , pib);
 printf("Pontos Turisticos: %d \n" , pontos_turisticos);
 
 
+printf("\n Cidade 2\n");
 
-return 0;
+
+
+printf("Digite Sua Cidade:\n");
+scanf("%s" , nome);
+
+printf("Digite A População Da Sua Cidade:\n");
+scanf("%f" , &populacao);
+
+printf("Digite A Áerea Da Sua Cidade:\n");
+scanf("%f" , &area);
+
+printf("Digite O Pib Da Sua Cidade:\n");
+scanf("%f" , &pib);
+
+printf("Numeros De Pontos Turistícos:\n");
+scanf("%d" , &pontos_turisticos);
+
+
+// Dados Da Cidade //
+
+printf("Nome Da Cidade: %s\n" , nome);
+printf("População: %f\n" , populacao);
+printf("Área: %f\n" , area);
+printf("Pib: %f\n" , pib);
+printf("Pontos Turistícos: %d\n" , pontos_turisticos);
+
+
+
+printf("Cidade 3 \n");
+
+
+printf("Digite Sua Cidade:\n");
+scanf("%s" , nome);
+
+printf("Digite A População Da Sua Cidade:\n");
+scanf("%f" , &populacao);
+
+printf("Digite A Área Da Sua Cidade:\n");
+scanf("%f" , &area);
+
+printf("Digite O Pib Da Sua Cidade:\n");
+scanf("%f" , &pib);
+
+printf("Pontos Turisticos:\n");
+scanf("%d" , &pontos_turisticos);
+
+
+
+// Dados Da Cidade //
+
+printf("Nome Da Cidade: %s\n" , nome);
+printf("População:%f\n" , populacao);
+printf("Área: %f\n" , area);
+printf("Pib: %f\n" , pib);
+printf("Pontos Turisticos: %d\n" , pontos_turisticos);
+
+
+
+
+
+
+printf("Cidade 4\n");
+
+
+
+printf("Digite Sua Cidade:\n");
+scanf("%s" , nome);
+
+printf("Digite A População Da Sua Cidade: \n");
+scanf("%f" , &populacao);
+
+printf("Digite A Área Da Sua Cidade: \n");
+scanf("%f" , &area);
+
+printf("Digite O Pib Da Sua Cidade: \n");
+scanf("%f" , &pib);
+
+printf("Pontos Turisticos: \n");
+scanf("%d" , &pontos_turisticos);
+
+
+// Dados Da Cidade //
+
+
+printf("Nome Da Cidade: %s\n" , nome);
+printf("População: %f\n" , populacao);
+printf("Área: %f\n" , area);
+printf("Pib: %f\n" , pib);
+printf("Pontos Turisticos: %d\n" , pontos_turisticos);
+
+
+
+
+   return 0;
 
 }
